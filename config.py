75e32fef5e859595822238766b96cc181c559df6
Vesa Kalliomäki <vesa.kalliomaki@student.careeria.fi>
2025-04-22 14:18:40 +0300

    siirrytään käyttämään renderin postgreSQL

--- config.py
+++ config.py
@@ -1,7 +1,13 @@
+import os
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = (
-        "mssql+pyodbc://adminAutonhuolto:Autonhuolto10%25@srvautonhuolto.database.windows.net:1433/autonhuolto?driver=ODBC+Driver+17+for+SQL+Server&encrypt=yes&trustservercertificate=no"
-        # "mssql+pyodbc://@DESKTOP-L26Q33J\\SQLEXPRESS/autonhuolto"
-        # "?driver=ODBC+Driver+17+for+SQL+Server&trusted_connection=yes"
-    )
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+# class Config:
+#     SQLALCHEMY_DATABASE_URI = (
+#         "postgresql://autonhuolto_user:Q8iBgQXBXkfF2ZQT4JfQsjbMfwfef2xG@dpg-d03nf8ali9vc73frn3j0-a/autonhuolto"
+#         # "mssql+pyodbc://@DESKTOP-L26Q33J\\SQLEXPRESS/autonhuolto"
+#         # "?driver=ODBC+Driver+17+for+SQL+Server&trusted_connection=yes"
+#     )
+#     SQLALCHEMY_TRACK_MODIFICATIONS = False
