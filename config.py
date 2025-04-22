2883854e69ef68f4454db28dc28c2e524719944d
Vesa Kalliomäki <vesa.kalliomaki@student.careeria.fi>
2025-04-22 12:13:30 +0300

    azure yhteys luotu

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 class Config:
     SQLALCHEMY_DATABASE_URI = (
-        "mssql+pyodbc://@DESKTOP-L26Q33J\\SQLEXPRESS/autonhuolto"
-        "?driver=ODBC+Driver+17+for+SQL+Server&trusted_connection=yes"
+        "mssql+pyodbc://adminAutonhuolto:Autonhuolto10%25@srvautonhuolto.database.windows.net:1433/autonhuolto?driver=ODBC+Driver+17+for+SQL+Server&encrypt=yes&trustservercertificate=no"
+        # "mssql+pyodbc://@DESKTOP-L26Q33J\\SQLEXPRESS/autonhuolto"
+        # "?driver=ODBC+Driver+17+for+SQL+Server&trusted_connection=yes"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
